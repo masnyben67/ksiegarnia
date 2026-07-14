8b59dc066d1ecdba7fb3d3a858af9129df5d8785
masnyben67 <zeg.zuus@gmail.com>
2026-07-14 12:10:34 +0200

    Create OpenApiConfig.java

---
+++ src/main/java/pl/ksiegarnia/online/config/OpenApiConfig.java
@@ -0,0 +1,51 @@
+package pl.ksiegarnia.online.config;
+
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+/*
+    Konfiguracja Swaggera.
+
+    Dzięki temu dostajemy
+    interaktywną dokumentację API.
+*/
+@Configuration
+public class OpenApiConfig {
+
+
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+
+
+        return new OpenAPI()
+
+                .info(
+
+                    new Info()
+
+                    .title(
+                        "Księgarnia Online API"
+                    )
+
+                    .version(
+                        "1.0"
+                    )
+
+                    .description(
+                        "API do obsługi koszyka i promocji"
+                    )
+
+                );
+
+    }
+
+
+}
